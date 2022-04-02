e1a6153939f377614459600620b9c07f57a80e88
Oscar GG <profesor.oscar.gomez@gmail.com>
2022-04-02 16:12:05 +0200

    Error en test

--- utilidades/test_vagrant.py
+++ utilidades/test_vagrant.py
@@ -7,9 +7,9 @@ gestor_vagrant.anadir_tarjeta_puente("enp0s25")
 gestor_vagrant.anadir_tarjeta_puente("enp0s25")
 gestor_vagrant.anadir_carpeta_compartida("en_anfitrion", "/invitado")
 gestor_vagrant.anadir_fichero_para_copiar("anfitrion.txt", "en_invitado.txt")
-gestor_vagrant.anadir_comando("apt-get update")
-gestor_vagrant.anadir_comando("apt-get upgrade")
-gestor_vagrant.anadir_comando("apt-get install apache2")
+gestor_vagrant.anadir_comando("apt-get update -y")
+gestor_vagrant.anadir_comando("apt-get upgrade -y")
+gestor_vagrant.anadir_comando("apt-get install apache2 -y")
 
 texto=gestor_vagrant.get_texto_vagrantfile()
 
